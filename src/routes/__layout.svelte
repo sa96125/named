46ae4f3da3a2dce5d4d8f6e1499aea068ca2e86b
jongseung park <sa96125@gmail.com>
2022-06-29 23:13:28 +0900

    ✨ add root page, layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { navigating } from '$app/stores';
+	import '../app.css';
+	import Nav from '$lib/components/Nav.svelte';
+	import PreloadingIndicator from '$lib/components/PreloadingIndicator.svelte';
+</script>
+
+{#if $navigating}
+	<PreloadingIndicator />
+{/if}
+
+<Nav />
+<div class=" max-w-full">
+	<slot />
+</div>
