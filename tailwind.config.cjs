dec8af2711b65b6741b6e1a9640e147cfe24060c
jongseung park <sa96125@gmail.com>
2022-06-30 23:58:01 +0900

    💄 update fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,15 @@
-
-const { fontFamily } = require('tailwindcss/defaultTheme')
+const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
 	content: ['./src/**/*.svelte', './src/**/*.css'],
 	theme: {
 		extend: {
-      fontFamily: {
-        display : ['noe' , ...fontFamily.sans],
-				sans : ['titillium' , ...fontFamily.sans],
-      },
-    }
+			fontFamily: {
+				display: ['noe', ...fontFamily.sans],
+				sans: ['titillium', ...fontFamily.sans],
+				studio: ['studio', ...fontFamily.sans]
+			}
+		}
 	},
 	variants: {
 		extend: {}
