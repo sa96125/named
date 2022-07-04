4b1a600d176605bf341f8c9bfe37f731072864ae
박종승 <sa96125@gmail.com>
2022-07-04 09:14:33 +0000

    Use PrismaClient

--- src/routes/api/auth/login.json.js
+++ src/routes/api/auth/login.json.js
@@ -1,18 +1,18 @@
 import { sign } from 'jsonwebtoken';
+import PrismaClient from "$lib/prisma"
 
-// TODO Persist in database
-const account = {
-	id: '1',
-	email: 'sa96125@gmail.com',
-	password: '1234'
-};
+const prisma = new PrismaClient();
 
 export const post = async ({ request }) => {
 	const data = await request.formData();
 
-	// TODO: Find ID
-	// - !user return no user error.
-	if (account.email !== data.get('email') || account.password !== data.get('password')) {
+	const user = await prisma.user.findUnique({
+		where: {
+		  email: data.get('email'),
+		}
+	})
+
+	if(!user) {
 		return {
 			status: 404,
 			headers: {
@@ -22,17 +22,11 @@ export const post = async ({ request }) => {
 		};
 	}
 
-	const body = {
-		user: {
-			id: '1'
-		}
-	};
-
 	// TODO: check password
 	// - !passwordCorrect  return incorrect error.
 
 	// TODO: return JWT(id)
-	const jwt = sign(body.user.id, import.meta.env.VITE_SECRET_KEY);
+	const jwt = sign(user.uid, import.meta.env.VITE_SECRET_KEY);
 	return {
 		status: 302,
 		headers: {
