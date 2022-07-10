57f68c50911b1f4def5cef23cd9556e365389576
jongseung park <sa96125@gmail.com>
2022-07-10 23:46:17 +0900

    Install & Config AutoImport

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,23 @@
 import adapter from '@sveltejs/adapter-auto';
+import autoImport from 'sveltekit-autoimport';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
+		alias: {
+			$components: 'src/lib/components',
+			$store: 'src/lib/store'
+		},
 		vite: {
+			plugins: [
+				autoImport({
+					components: ['./src/lib/components'],
+					module: {
+						svelte: ['onMount']
+					}
+				})
+			],
 			server: {
 				hmr: {
 					clientPort: process.env.HMR_HOST ? 443 : 3000,
