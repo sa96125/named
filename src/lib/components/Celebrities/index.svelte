3a672b1495a02c8bbabc4d9f691f962531243db2
jongseung park <sa96125@gmail.com>
2022-07-10 23:50:31 +0900

    Add useViewportAction(I-O) for Infinity Scroll.

--- src/lib/components/Celebrities/index.svelte
+++ src/lib/components/Celebrities/index.svelte
@@ -1,16 +1,46 @@
 <script>
-	import { session } from '$app/stores';
-	import CelebrityPreview from './CelebrityPreview.svelte';
+	import { navigating } from '$app/stores';
+	import viewport from '../../actions/useViewportAction';
 	export let celebrities;
 
+	const celebrityPreviewPromise = import('./CelebrityPreview.svelte')
+
+	let page = 0;
+
+	$: if (!!$navigating) {
+		page = 0;
+	}
+
+	const getNextCelebrities = async () => {
+		page += 1;
+
+		const { celebrities: nextCelebrities } = await fetch(`/api/celebrities/?page=${page}`, {
+			credentials: 'include'
+		}).then((response) => response.json());
+
+		celebrities = [...celebrities, ...nextCelebrities];
+		console.log(nextCelebrities);
+	};
 </script>
 
 {#if celebrities.length === 0}
 	<div class="article-preview">No articles are here... yet.</div>
 {:else}
-	<div>
-		{#each celebrities as celebrity (celebrity.id)}
-			<CelebrityPreview {celebrity}  />
-		{/each}
-	</div>
+	{#each celebrities as celebrity, index (celebrity.id)}
+		{#await celebrityPreviewPromise}
+			<SkeletonCard />
+		{:then { default: CelebrityPreview }}
+			<CelebrityPreview {celebrity} />
+		{/await}
+		<SkeletonCard />
+		<SkeletonCard />
+		<SkeletonCard />
+		<SkeletonCard />
+		<SkeletonCard />
+		<SkeletonCard />
+		<SkeletonCard />	
+		{#if celebrities.length - 1 === index && celebrities.length >= 5}
+			<div use:viewport on:enterViewport={() => getNextCelebrities()} />
+		{/if}
+	{/each}
 {/if}
