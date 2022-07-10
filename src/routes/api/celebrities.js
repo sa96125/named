dd274cc1a25ac00ed3a1922ff5188f231c824f83
jongseung park <sa96125@gmail.com>
2022-07-10 23:51:24 +0900

    Update Get API

--- src/routes/api/celebrities.js
+++ src/routes/api/celebrities.js
@@ -4,11 +4,12 @@ const prisma = new PrismaClient();
 
 export async function get({ url: { searchParams } }) {
 	const tag = searchParams.get('tag');
+	const page = searchParams.get('page') || 0;
 	// const page = +searchParams.get('page') || 1;
 	// const jwt = locals.user && locals.user.token;
 	// pages: Math.ceil(articlesCount / page_size)
 	let celebrities;
-	
+
 	if (tag) {
 		celebrities = await prisma.celebrity.findMany({
 			where: {
@@ -21,14 +22,17 @@ export async function get({ url: { searchParams } }) {
 			include: {
 				tags: true,
 				platforms: true
-			}
+			},
+			take: 5
 		});
 	} else {
 		celebrities = await prisma.celebrity.findMany({
+			skip: 5 * page,
 			include: {
 				tags: true,
 				platforms: true
-			}
+			},
+			take: 5
 		});
 	}
 
