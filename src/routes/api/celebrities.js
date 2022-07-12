426fa2d3fea4491c9e87a8eb10c4d8ad2d1489d6
jongseung park <sa96125@knowck.com>
2022-07-12 17:18:12 +0900

    Add Prisma middleware for Logging

--- src/routes/api/celebrities.js
+++ src/routes/api/celebrities.js
@@ -1,6 +1,34 @@
 import PrismaClient from '$lib/prisma';
+// import Redis from "ioredis";
+// import { createPrismaRedisCache } from "prisma-redis-middleware";
 
+
+// const redis = new Redis();
 const prisma = new PrismaClient();
+
+// const cacheMiddleware = createPrismaRedisCache({
+//   models: [
+//     { model: "Celebrity", excludeMethods: ["findMany"] },
+//   ],
+//   storage: { type: "memory", options: { size: 2048 }, log: console },
+//   cacheTime: 300,
+//   excludeModels: ["Product", "Cart"],
+//   excludeMethods: ["count", "groupBy"],
+//   onHit: (key) => console.log("hit", key),
+//   onMiss: (key) => console.log("miss", key),
+//   onError: (key) => console.log("error", key),
+// });
+
+const loggingMiddleware = async (params, next) => {
+  const before = Date.now()
+  const result = await next(params)
+  const after = Date.now()
+  console.log(`Query ${params.model}.${params.action} took ${after - before}ms`)
+  return result
+}
+
+// prisma.$use(cacheMiddleware);
+prisma.$use(loggingMiddleware)
 
 export async function get({ url: { searchParams } }) {
 	const tag = searchParams.get('tag');
