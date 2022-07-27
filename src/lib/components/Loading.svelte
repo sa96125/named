e17fe7b02753613473e5f54d8bf2c17d3f30ebe1
jongseung park <sa96125@gmail.com>
2022-07-27 23:41:34 +0900

    Update Page Lazy Loading

--- src/lib/components/Loading.svelte
+++ src/lib/components/Loading.svelte
@@ -4,12 +4,16 @@
 	$: if ($loading.status === 'NAVIGATING') {
 		setTimeout(() => {
 			if ($loading.status === 'NAVIGATING') {
-				$loading.status = 'LOADING';
+				loading.setLoading(true);
 			}
-		}, 200);
+		}, 400);
 	}
 </script>
 
 {#if $loading.status === 'LOADING'}
-	<div class="absolute w-full h-full flex-center">loading</div>
+	<div class="z-10 absolute top-0 left-0 w-full h-full flex-center flex-col">
+		<div class="p-10 text-[#f11919] text-4xl font-extrabold font-display flex-center flex-col">
+			<p>L O A D I N G</p>
+		</div>
+	</div>
 {/if}
