1f69a13ad1616437e737c8fc27e7ab77721f146e
jongseung park <sa96125@gmail.com>
2022-07-27 23:52:48 +0900

    Fix path

--- src/services/middleware/useCachePage.js
+++ src/services/middleware/useCachePage.js
@@ -1,10 +1,10 @@
 import { getCachedPage, setCachedPage } from '../../services/queries/page-caches.js';
-import { streamToString } from '$lib/util/streamToString.js';
+import { streamToString } from '../../lib/util/streamToString.js';
 
 const cacheableRoutes = ['/', '/login', '/sign-up'];
 
 export const useCachePage = async ({ event, resolve }) => {
-	console.log(event.url.pathname)
+	console.log(event.url.pathname);
 	if (!cacheableRoutes.includes(event.url.pathname)) {
 		return resolve(event);
 	}
